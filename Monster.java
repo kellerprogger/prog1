216b36e92a147876f81d966400705ed3905ed434
Tyrfingr <Tyrfingr@users.noreply.github.com>
2015-01-01 14:37:14 +0100

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,42 @@
+public class Monster{
+    int maxHp;
+    int hp;
+    int atk;
+    double hitChance;
+    
+    public Monster(int maxHp, int atk, double hitChance){
+        this.maxHp = maxHp;
+        this.atk = atk;
+        this.hitChance = hitChance;
+        hp = maxHp;
+    }
+    public boolean isDefeated(){
+        if(hp == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }    
+    public void takeDamage(int damage){
+        hp -= damage;
+        if (hp < 0){
+            hp = 0;
+        }    
+    }
+    public int attack(Player player){
+        if (hitchance <= Math.random()){
+            return -1;
+        } else {
+            int x = int(atk*Math.random() )
+            monster.takeDamage(x);
+            return x;
+        }
+    }
+    public String toString(){
+        System.out.println("---------------------.");
+        System.out.println("HP: "+ hp);
+        System.out.println("ATK: "+ atk);
+        System.out.println("----------------------");
+    }
+}
