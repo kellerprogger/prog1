ff56815c19f71f110ee3d732f2dcc5de1388081d
Tyrfingr <Tyrfingr@users.noreply.github.com>
2015-01-02 16:36:18 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,16 +1,17 @@
+import java.util.Scanner;
 public class Game{
     public static void main(String [] döner){
-        
+
         Player spieler = new Player(100, 50, 42, 4, 0.5, 20, 3);
-        
+
         Monster m1 = new Monster(100, 30, 0.4);
         Monster m2 = new Monster(120, 10, 0.3);
         Monster m3 = new Monster(420, 7, 0.6);
         Monster m5 = new Monster(123, 10, 0.11);
         Monster m4 = new Monster(666, 40, 0.01);
-        
+
         double rand = Math.random();
-        
+
         if (rand < 0.2){
             fight(spieler, m1);
         }
@@ -26,10 +27,38 @@ public class Game{
         else {
             fight(spieler, m5);
         }
-        
+
     public void fight(Player player, Monster monster){
-        
-        
+        Scanner scan = new Scanner();
+        while (true){
+            System.out.println(Player.toString());
+            System.out.println(Monster.toString());
+            System.out.println(Player.actions());
+            String input = scan.nextLine();
+            if (input.equals("1")){
+                dmg = Player.attack(monster))
+                if(atk == -1){
+                    System.out.println("Attack Missed");
+                } else {
+                    System.out.println("You've dealt: " + dmg + " Damage" );
+                }
+            } else if (input.equals("2")){
+                boolean heal = Player.heal();
+                if (heal == true ){
+                    System.out.println("You where healed by: " 
+                    +  Player.healingPower);
+                } else {
+                    System.out.println("Healing Failed!");
+                }
+
+            } else if (input.equals("3")){
+
+            } else {
+                System.out.println("Invalid Action")
+            }
+
+        }
+
     }
-    }
+
 }
