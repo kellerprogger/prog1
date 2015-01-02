4e56e162ec54bdec15033fd503688711e413dae9
Tyrfingr <Tyrfingr@users.noreply.github.com>
2015-01-02 11:31:54 +0100

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,28 +1,15 @@
-public class Player{
+public class Monster{
     int maxHp;
     int hp;
     int atk;
-    int healingPower;
-    int remainingItemUses;
     double hitChance;
-    int ap;
-    int maxAp;
-    int apRegen;
-
-
-    public Player(int maxHp, int atk, int healingPower, int remainingItemUses,
-                    double hitChance, int maxAp, int apRegen){
+    
+    public Monster(int maxHp, int atk, double hitChance){
         this.maxHp = maxHp;
         this.atk = atk;
-        this.healingPower = healingPower;
-        this.remainingItemUses = remainingItemUses;
         this.hitChance = hitChance;
-        this.maxAp = maxAp;
-        this.apRegen = apRegen;
         hp = maxHp;
-        ap = maxAp;
     }
-    
     public boolean isDefeated(){
         if(hp == 0){
             return true;
@@ -31,15 +18,13 @@ public class Player{
             return false;
         }
     }
-    
     public void takeDamage(int damage){
         hp -= damage;
         if (hp < 0){
             hp = 0;
         }
     }
-    
-    public int attack(Monster monster){
+    public int attack(Player player){
         if (hitchance <= Math.random()){
             return -1;
         } else {
@@ -48,56 +33,10 @@ public class Player{
             return x;
         }
     }
-    
-    public int getRemainingItemUses(){
-        return remainingItemUses;
-    }
-    
-    public boolean heal(){
-        if (remainingItemUses > 0){
-            remainingItemUses -= 1;
-            hp += healingPower;
-            if (hp > maxHp){
-                hp = maxHp;
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
     public String toString(){
         System.out.println("---------------------.");
         System.out.println("HP: "+ hp);
         System.out.println("ATK: "+ atk);
-        System.out.println("Remaining Items: "+ getRemainingItemUses());
         System.out.println("----------------------");
     }
-    
-    public int regenerateAp(){
-        ap += apRegen;
-        if (ap < maxAp){
-            return ap;
-        } else{
-            return maxAp;
-        }
-    }
-    
-    public int cast(int spell, Monster monster){
-        int dmg;
-        int cost;
-        switch (spell){
-            case 1: dmg = 10; cost = 5;
-            case 2: dmg = 20; cost = 13;
-            case 3: dmg = 30; cost = 19;
-        }
-        if (ap >= cost){
-            monster.takeDamage(dmg);
-            ap -= cost;
-            return dmg;
-        } else {
-            return -1;
-        }
-    }
-
 }
