16e23c81e0db750e5bcf25a2c8571c16ba4f4c11
Tyrfingr <Tyrfingr@users.noreply.github.com>
2015-01-02 11:32:16 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -9,7 +9,7 @@ public class Player{
     int maxAp;
     int apRegen;
 
-    
+
     public Player(int maxHp, int atk, int healingPower, int remainingItemUses,
                     double hitChance, int maxAp, int apRegen){
         this.maxHp = maxHp;
@@ -22,6 +22,7 @@ public class Player{
         hp = maxHp;
         ap = maxAp;
     }
+
     public boolean isDefeated(){
         if(hp == 0){
             return true;
@@ -30,12 +31,14 @@ public class Player{
             return false;
         }
     }
+
     public void takeDamage(int damage){
-        this.hp -= damage;
+        hp -= damage;
         if (hp < 0){
             hp = 0;
         }
     }
+
     public int attack(Monster monster){
         if (hitchance <= Math.random()){
             return -1;
@@ -45,13 +48,15 @@ public class Player{
             return x;
         }
     }
+
     public int getRemainingItemUses(){
         return remainingItemUses;
     }
+
     public boolean heal(){
         if (remainingItemUses > 0){
             remainingItemUses -= 1;
-            hp += healingPower
+            hp += healingPower;
             if (hp > maxHp){
                 hp = maxHp;
             }
@@ -60,6 +65,7 @@ public class Player{
             return false;
         }
     }
+
     public String toString(){
         System.out.println("---------------------.");
         System.out.println("HP: "+ hp);
@@ -67,6 +73,7 @@ public class Player{
         System.out.println("Remaining Items: "+ getRemainingItemUses());
         System.out.println("----------------------");
     }
+
     public int regenerateAp(){
         ap += apRegen;
         if (ap < maxAp){
@@ -76,4 +83,21 @@ public class Player{
         }
     }
 
-}   
+    public int cast(int spell, Monster monster){
+        int dmg;
+        int cost;
+        switch (spell){
+            case 1: dmg = 10; cost = 5;
+            case 2: dmg = 20; cost = 13;
+            case 3: dmg = 30; cost = 19;
+        }
+        if (ap >= cost){
+            monster.takeDamage(dmg);
+            ap -= cost;
+            return dmg;
+        } else {
+            return -1;
+        }
+    }
+
+}
