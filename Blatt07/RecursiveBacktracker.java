618e00e4f977d218cede96b66c5e278a643fb91d
Tyrfingr <bartuni38@googlemail.com>
2015-01-24 18:44:30 +0100

    xxx

--- Blatt07/RecursiveBacktracker.java
+++ Blatt07/RecursiveBacktracker.java
@@ -1,45 +1,63 @@
 public class RecursiveBacktracker implements MazeGenerator {
-    
-    char[][] map = {{'#','#','#','#','#','#','#','#','#'},
-                    {'#',' ','#',' ','#',' ','#',' ','#'},
-                    {'#','#','#','#','#','#','#','#','#'},
-                    {'#',' ','#',' ','#',' ','#',' ','#'},
-                    {'#','#','#','#','#','#','#','#','#'}
-                    };
-    
-    boolean[][] pos = new boolean[map.length()][map[].length()];
+
+    private boolean[][] visitedField = new boolean[map.length()][map[].length()];
 
     public char[][] generate(int y, int x){
-                  
-        yMod = randDirection()[0];
-        xMod = randDirection()[1];
-        
-        if (pos[y][x] == true){
-            
-        } else {
-            pos[y][x] = true;
-            map[y][x] = '.';
-            generate(y + yMod, x + xMod);
+
+        char[][] map = {{'#','#','#','#','#','#','#','#','#'},
+                        {'#',' ','#',' ','#',' ','#',' ','#'},
+                        {'#','#','#','#','#','#','#','#','#'},
+                        {'#',' ','#',' ','#',' ','#',' ','#'},
+                        {'#','#','#','#','#','#','#','#','#'}
+                        };
+    }
+
+    public boolean generator(int x, int y){
+        while(freeField(x,y)){
+            int[] field = randomField(x,y);
+            removeWall(field[3], x, y); //field[3] holds orientation
+            setVisitedField(field);
+            generator(field[0], field[1]);
+
         }
-        return map;
-    }    
-    
-    public int[] randDirection() {
-        rand = math.Random();
-        
-        if (rand <= 0.25) {
-            return int[] i = {0, 1}; // x = 1
-        
-        } else if (rand <= 0.5) {
-            return int[] i = {1, 0}; // y = 1
-        
-        } else if (rand <= 0.75) {
-            return int[] i = {0, -1}; // x = -1 
-        
-        } else {
-            return int[] i = {-1, 0}; // y = -1
+        return true;
+    }
+    private boolean freeField(int x, int y){
+        if (!getVisitedField(x + 1, y)){ // If field Free return true
+            return true;
+        } else if (!getVisitedField(x - 1, y)){
+            return true;
+        } else if (!getVisitedField(x, y + 1)){
+            return true;
+        } else if (!getVisitedField(x, y - 1)){
+            return true;
+        else{
+            return false;
         }
     }
+    private boolean getVisitedField(int x, int y){
+        if (visitedField[y][x]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    private void setVisitedField(int x, int y){
+        visitedField[y][x] = true;
+    }
+    private void removeWall(int orientation, int x, int y){
+        if (orientation == 0){ // North
+            map[y][x + 1] = '.';
+        } else if (orientation == 1){ //South
+            map[y][x - 1] = '.';
+        } else if (orientation == 2){ //West
+            map[y + 1][x] = '.';
+        } else if (orientation == 3){ //East
+            map[y - 1][x] = '.';
+        }
+    }
+    private int[]randomField(int x, int y){
+        
+    }
 }
-
-
