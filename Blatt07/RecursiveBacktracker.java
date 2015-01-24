0a4154b6fcecf1e892f250deb635b200ed4fd280
Tyrfingr <bartuni38@googlemail.com>
2015-01-24 16:22:30 +0100

    ...

--- Blatt07/RecursiveBacktracker.java
+++ Blatt07/RecursiveBacktracker.java
@@ -1,22 +1,29 @@
 public class RecursiveBacktracker implements MazeGenerator {
-
-char[][] map = {{'#','#','#','#','#','#','#','#','#'},
-                        {'#',' ','#',' ','#',' ','#',' ','#'},
-                        {'#','#','#','#','#','#','#','#','#'},
-                        {'#',' ','#',' ','#',' ','#',' ','#'},
-                        {'#','#','#','#','#','#','#','#','#'}
-                      };
-
+    
+    char[][] map = {{'#','#','#','#','#','#','#','#','#'},
+                    {'#',' ','#',' ','#',' ','#',' ','#'},
+                    {'#','#','#','#','#','#','#','#','#'},
+                    {'#',' ','#',' ','#',' ','#',' ','#'},
+                    {'#','#','#','#','#','#','#','#','#'}
+                    };
+    
+    boolean[][] pos = new boolean[map.length()][map[].length()];
 
     public char[][] generate(int y, int x){
+                  
+        yMod = randDirection()[0];
+        xMod = randDirection()[1];
+        
         if (pos[y][x] == true){
             
         } else {
             pos[y][x] = true;
-
+            map[y][x] = '.';
             generate(y + yMod, x + xMod);
         }
+        return map;
     }    
+    
     public int[] randDirection() {
         rand = math.Random();
         
